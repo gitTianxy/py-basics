002642d9f947328267cac0c38da85b60ccad5a99
田欣瑜 KevinTian <kevintian@synacast.local>
2017-07-10 10:12:11 +0800

    add: list_sort

--- list_demo.py
+++ list_demo.py
@@ -2,15 +2,27 @@
 
 import random
 
+# prepare data
 list1 = []
+dics = []
+tuples = []
 for i in range(0, 10):
-    list1.append(random.randrange(0, 100))
+    val = random.randrange(0, 100)
+    list1.append(val)
+    dics.append({'key%s' % i: val})
+    tuples.append(('key%s' % i, val))
 
+# do sort
+print 'dics_before_sort: ', dics
+dics.sort(key=lambda f: f.values()[0])
+print 'dics_after_sort: ', dics
+print 'tuples_before_sort: ', tuples
+tuples.sort(key=lambda f: f[1])
+print 'tupels_after_sort: ', tuples
+# test sort
+print 'list1_before_copy_sort: ', list1
 list1_copy = list(list1)
 list1_copy.sort()
-print "-------- list1 ---------"
-for i1 in list1:
-    print i1
-print "-------- sorted list1_copy ---------"
-for i2 in list1_copy:
-    print i2
+print 'list1_after_copy_sort: ', list1
+print 'list1_copy_after_sort: ', list1_copy
+
