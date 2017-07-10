63a562688fd03f4459d0a30e1d37ace40aaca91f
田欣瑜 KevinTian <kevintian@synacast.local>
2017-07-10 20:33:18 +0800

    ...

--- threadpool_demo2.py
+++ threadpool_demo2.py
@@ -14,10 +14,10 @@ def process(item):
     sleep(1)
 
 mutex = threading.Lock()
-items = range(0, 100)
+items = range(0, 50)
 print 'pool_default-----------------'
 pool_default = ThreadPool()
 pool_default.map(process, items)
 print 'pool_set-----------------'
-pool_set = ThreadPool(10)
+pool_set = ThreadPool(50)
 pool_set.map(process, items)
