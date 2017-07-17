84c283cfbab6672daaa967f0973ef98e3e67013d
田欣瑜 KevinTian <kevintian@synacast.local>
2017-07-17 11:39:38 +0800

    rev:file-write

--- file_write.py
+++ file_write.py
@@ -1,25 +1,36 @@
 # coding=utf-8
 import xlwt
 
-path = "file-demo.txt"
-line = "hello, this is a write-line"
+line = None
 lines = []
-for i in range(0, 10):
-    lines.append("\nthis is line %s" % i)
 
-with open(path, "w") as f:
-    f.write(line)
-    f.writelines(lines)
-f.close()
+def init_content():
+    global lines
+    global line
+    line = "hello, this is a write-line"
+    for i in range(0, 10):
+        lines.append("\nthis is line %s" % i)
 
 
-def write_excel():
+def write_txt(path):
+    global lines
+    global line
+    with open(path, "w") as f:
+        f.write(line)
+        f.writelines(lines)
+    f.close()
+
+
+def write_excel(path):
     global lines
     workbook = xlwt.Workbook(encoding='ascii')
     worksheet = workbook.add_sheet('My Worksheet')
     for idx in range(0, len(lines)):
         worksheet.write(idx, 0, label=lines[idx])
-    workbook.save('result/file_excel.xls')
+    workbook.save(path)
 
-if __name__=="__main__":
-    write_excel()
+
+if __name__ == "__main__":
+    init_content()
+    write_txt('result/file_txt.txt')
+    write_excel('result/file_excel.xls')
