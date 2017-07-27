4fae0cc733d2b4d4d7adc55d8613c09c07c689b0
kevintian <kevintian@pptv.com>
2017-07-27 10:44:03 +0800

    update exception

--- exception.py
+++ exception.py
@@ -1,4 +1,3 @@
-
 print "------python use 'try-except' to CATCH exception, and use 'raise' to THROW exception-----"
 
 try:
@@ -15,3 +14,34 @@ except ArithmeticError, e:
     print "ArithmeticError: ", e.message
 except Exception, e:
     print "Exception: ", e.message
+
+print '---------------------------'
+err_arr = [ValueError("value-err"), AttributeError("attr-err"), Exception("exception"), RuntimeError("runtime-err")]
+import logging
+def init_log():
+    # stdout
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)
+
+def do_tsk(idx):
+    try:
+        raise err_arr[idx]
+    except Exception, e:
+        raise e
+
+def do_job():
+    init_log()
+    for i in range(0, len(err_arr)):
+        try:
+            do_tsk(i)
+        except Exception, e:
+            err = "tsk %s error: %s" % (i, e.message)
+            logging.info(err)
+            #logging.error(err)
+        info = "tsk %s finish" % i
+        logging.info(info)
+
+do_job()
