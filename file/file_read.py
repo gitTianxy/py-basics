a5647628ccd6c7e104d913c20981dc57e682e449
gitTianxy <txy_learning@163.com>
2017-09-29 15:32:04 +0800

    udpate: excel operation

--- file/file_read.py
+++ file/file_read.py
@@ -1,4 +1,5 @@
 import json
+import xlrd
 
 
 def loadJson(jsonPath):
@@ -6,8 +7,28 @@ def loadJson(jsonPath):
         return json.load(json_file)
 
 
+def read_excel():
+    path = '../result/file_excel.xls'
+    workbook = xlrd.open_workbook(path)
+    sheet1 = workbook.sheet_by_index(0)
+    nrows = sheet1.nrows
+    ncols = sheet1.ncols
+    # get all cell
+    for rowx in range(0, nrows):
+        for colx in range(0, ncols):
+            print sheet1.cell(rowx, colx).value
+    # get row
+    for rowx in range(0, nrows):
+        print sheet1.row_values(rowx)
+    # get column
+    for colx in range(0, ncols):
+        print sheet1.col_values(colx)
+
+
 if __name__ == '__main__':
     # load json
     data = loadJson('../data/statistic_170616-170623.json')
     for item in data:
         print "fid: ", item['fid']
+    # read excel
+    read_excel()
--- file/file_write.py
+++ file/file_write.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 import xlwt
+from xlutils.copy import copy
+from xlrd import open_workbook
 
 line = None
 lines = []
+
 
 def init_content():
     global lines
@@ -21,16 +24,42 @@ def write_txt(path):
     f.close()
 
 
-def write_excel(path):
+def create_excel(path):
     global lines
-    workbook = xlwt.Workbook(encoding='ascii')
+    workbook = xlwt.Workbook(encoding='utf-8') # ascii
     worksheet = workbook.add_sheet('My Worksheet')
     for idx in range(0, len(lines)):
         worksheet.write(idx, 0, label=lines[idx])
     workbook.save(path)
 
 
+def append_excel(path):
+    """
+    append column or row
+    :param path:
+    :return:
+    """
+    # open file
+    book_ro = open_workbook(path)
+    sheet = book_ro.sheet_by_index(0)
+    nrows = sheet.nrows
+    ncols = sheet.ncols
+    # creates a writeable copy
+    book = copy(book_ro)
+    sheet1 = book.get_sheet(0)
+    # append column
+    for colx in range(ncols, ncols+2):
+        for rowx in range(0, nrows):
+            sheet1.write(rowx, colx, 'new colume %s' % colx)
+    # append row
+    for rowx in range(nrows, nrows+10):
+        sheet1.write(rowx, 0, 'new row %s' % rowx)
+    # save
+    book.save(path)
+
+
 if __name__ == "__main__":
     init_content()
-    write_txt('result/file_txt.txt')
-    write_excel('result/file_excel.xls')
+    write_txt('..result/file_txt.txt')
+    create_excel('..result/file_excel.xls')
+    append_excel('../result/file_excel.xls')
