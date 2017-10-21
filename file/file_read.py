2be9d2c50f7d135b70e7ee82d834b46fcf36e8d0
kevintian <kevintian@pptv.com>
2017-10-21 14:40:36 +0800

    add: read .txt

--- file/file_read.py
+++ file/file_read.py
@@ -6,8 +6,25 @@ def loadJson(jsonPath):
         return json.load(json_file)
 
 
+def readlines_txt(path):
+    with open(path) as f:
+        return [l.rstrip() for l in f.readlines()]
+
+
+def read_txt(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
 if __name__ == '__main__':
+    root = "../data/"
     # load json
-    data = loadJson('../data/statistic_170616-170623.json')
+    data = loadJson(root + 'statistic_170616-170623.json')
     for item in data:
         print "fid: ", item['fid']
+    # readlines from .txt
+    lines = readlines_txt(root + "pgc-cid.txt")
+    for l in lines:
+        print l
+    print '------------------'
+    print read_txt(root + "pgc-cid.txt")
